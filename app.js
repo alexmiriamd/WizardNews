5a91856432c05b342c5b8cf184365c1546df144e
TheHighnote <ryanhighnote7@gmail.com>
2023-02-27 13:34:35 -0600

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const posts = require("./postBank")
+const list = posts.list
 
 app.use(morgan('dev')); //after installing morgan, logs req and res info
 
-app.get("/", (req, res) => res.send("Hello World!!"));
+app.get("/", (req, res) => {list})
+console.log(list)
 
 const PORT = 1337;
 
