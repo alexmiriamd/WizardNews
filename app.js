06a6c337ddecc5d8355120b12826e25683a5204b
Alexandra <alexmiriamd@gmail.com>
2023-02-27 12:02:28 -0700

    morgan installed and working

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(morgan('dev')); //after installing morgan, logs req and res info
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World!!"));
 
 const PORT = 1337;
 
