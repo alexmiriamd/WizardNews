9277ebe8d9881fc1443eed5aafb82adaf9cda0da
Alexandra <alexmiriamd@gmail.com>
2023-02-28 11:36:43 -0700

    deployment process started

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get("/posts/:id", (req, res) => {   //get single posts
   res.send(postDetails(post));
 });
 
-const PORT = 1337;
+const {PORT = 1337} = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
